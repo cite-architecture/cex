a654bf9f084a08972e069a2b07ceef2d1b634535
Neel Smith <dnsmith.neel@gmail.com>
2017-05-25 16:38:00 -0400

    Add api docs

--- jvm/src/main/scala/CexParserSource.scala
+++ jvm/src/main/scala/CexParserSource.scala
@@ -2,8 +2,18 @@ package edu.holycross.shot.cex
 
 import scala.io.Source
 
+
+/** Factory for creating [[CexParser]]s for CEX data
+* accessible from JVM-specific sources.
+*/
 object CexParserSource {
 
+
+  /** Create a [[CexParser]] for data in a file
+  * in the local file system.
+  *
+  * @param fileName Name of file with CEX source data.
+  */
   def fromFile(fileName: String): CexParser = {
     val lns = Source.fromFile(fileName).getLines.toVector
     CexParser(lns.mkString("\n"))

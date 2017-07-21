62506304b47b42b73cae708f1b40051cf3076c96
Neel Smith <dnsmith.neel@gmail.com>
2017-07-21 09:27:28 -0400

    Add package docs

--- shared/src/main/scala/edu/holycross/shot/cex/package.scala
+++ shared/src/main/scala/edu/holycross/shot/cex/package.scala
@@ -1,7 +1,36 @@
 package edu.holycross.shot
 
+
+/** Provides an object for parsing citable resources in CEX serialization.
+* See https://github.com/cite-architecture/citedx for documentation
+* of the CEX format.
+*
+* ==Overview==
+* The main class is the [[CexParser]].  It is constructucted from
+* a string in CEX format, and provides functions for working with
+* distinct blocks of the data source.  For example:
+*
+* {{{
+* val cex = CexParser(cexSourceString)
+* // get a map of all blocks labels to a Vector of one or
+* // more string data sets:
+* val blockMap = cex.blockMap
+* // get a single string concatenating all data for a single
+* // block type:
+* val ctsCatalog = cex.blockString("ctscatalog")
+* }}}
+*
+* In the JVM environment, the [[CexParserSource]] object provides
+* a single-step factory function for creating parsers from files:
+*
+* {{{
+* val cex = CexParserSource.fromFile("filename.cex")
+* }}}
+*/
 package object cex {
 
+  /** Exhaustive set of valid labels for CEX blocks.
+  */
   val labels = Set(
     "cexversion",
     "citelibrary",
